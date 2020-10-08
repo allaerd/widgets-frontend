c3d6491ad754abb22841fc37a64bfaa59642234b
Allaerd Mensonides <info@allaerd.org>
2020-10-08 16:17:44 +0200

    log in guard

--- src/app/widgets/widgets.routes.ts
+++ src/app/widgets/widgets.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
-import { WidgetDetailComponent } from './widget-detail/widget-detail.component';
-import { WidgetListComponent } from './widget-list/widget-list.component';
+import {Routes} from '@angular/router';
+import {WidgetDetailComponent} from './widget-detail/widget-detail.component';
+import {WidgetListComponent} from './widget-list/widget-list.component';
+import {LoggedInGuard} from '../core/guards/logged-in.guard';
 
 export const WidgetsRoutes: Routes = [
-  {path: 'widgets', component: WidgetListComponent},
-  {path: 'widgets/create', component: WidgetDetailComponent},
-  {path: 'widgets/edit/:id', component: WidgetDetailComponent},
+    {path: 'widgets', component: WidgetListComponent, canActivate: [LoggedInGuard]},
+    {path: 'widgets/create', component: WidgetDetailComponent, canActivate: [LoggedInGuard]},
+    {path: 'widgets/edit/:id', component: WidgetDetailComponent, canActivate: [LoggedInGuard]},
 ];
